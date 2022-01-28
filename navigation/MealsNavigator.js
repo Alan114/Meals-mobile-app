d9441018c22647339b5e555690f2636fd9920123
Alan Pecenkovic <alanquad@yahoo.com>
2022-01-28 16:33:28 +0100

    tried to make bottomTabNavigator work but it won't

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,7 +1,9 @@
+import React from "react";
 import { Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 // import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
@@ -31,9 +33,38 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-// const MealsFavTabNavigator = createBottomTabNavigator({
-//   Meals: MealsNavigator,
-//   Favorites: FavoriteScreen,
-// });
+// const MealsFavTabNavigator = createBottomTabNavigator(
+//   {
+//     Meals: {
+//       screen: MealsNavigator,
+//       navigationOptions: {
+//         tabBarIcon: (tabInfo) => {
+//           return (
+//             <Ionicons
+//               name="ios-restaurant"
+//               size={25}
+//               color={tabInfo.tintColor}
+//             />
+//           );
+//         },
+//       },
+//     },
+//     Favorites: {
+//       screen: FavoriteScreen,
+//       navigationOptions: {
+//         tabBarIcon: (tabInfo) => {
+//           return (
+//             <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
+//           );
+//         },
+//       },
+//     },
+//   },
+//   {
+//     tabBarOptions: {
+//       activeTintColor: Colors.accentColor,
+//     },
+//   }
+// );
 
 export default createAppContainer(MealsNavigator);
