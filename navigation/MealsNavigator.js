b635d79bcbd8a801d74937ce656bb33ac665f117
Alan Pecenkovic <alanquad@yahoo.com>
2022-01-28 16:06:00 +0100

    react-navigation-tabs didn't install, so I downgraded dependencies for it to install after which the app crashed, reverted dependencies and commented out code that concerns bottomtabnavigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,9 +1,11 @@
 import { Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
+// import { createBottomTabNavigator } from "react-navigation-tabs";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
+import FavoriteScreen from "../screens/FavoritesScreen";
 import Colors from "../constants/Colors";
 
 const MealsNavigator = createStackNavigator(
@@ -28,5 +30,10 @@ const MealsNavigator = createStackNavigator(
     },
   }
 );
+
+// const MealsFavTabNavigator = createBottomTabNavigator({
+//   Meals: MealsNavigator,
+//   Favorites: FavoriteScreen,
+// });
 
 export default createAppContainer(MealsNavigator);
