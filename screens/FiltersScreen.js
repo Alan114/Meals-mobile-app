8df9b2a0333b01dc3312278bd4239e0d16b00f11
Alan Pecenkovic <alanquad@yahoo.com>
2022-01-31 16:08:43 +0100

    Adding content to the FiltersScreen component

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,12 +1,24 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Switch, Platform } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
+import Colors from "../constants/Colors";
 
 const FiltersScreen = (props) => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+
   return (
     <View style={styles.screen}>
-      <Text>The Filter Screen!</Text>
+      <Text style={styles.title}>Available Filters / Restrictions</Text>
+      <View style={styles.filterContainer}>
+        <Text>Gluten-free</Text>
+        <Switch
+          trackColor={{ true: Colors.primaryColor }}
+          thumbColor={Platform.OS === "android" ? Colors.primaryColor : ""}
+          value={isGlutenFree}
+          onValueChange={(newValue) => setIsGlutenFree(newValue)}
+        />
+      </View>
     </View>
   );
 };
@@ -32,8 +44,19 @@ FiltersScreen.navigationOptions = (navData) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    margin: 20,
+    textAlign: "center",
+  },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
   },
 });
 export default FiltersScreen;
